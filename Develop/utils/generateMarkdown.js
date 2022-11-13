8e44e05e53464ee954c0f62501bab17b9d055a2a
Alex Frankhouser <freesoul423@gmail.com>
2022-11-13 14:34:12 -0500

    Added a switch statement for the lisence badges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,21 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseSection(license) {
+  switch (license) {
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)'
+      
+    case 'Apache 2.0':
+      return '[![License](https://img.shields.io/badge/License-Apache_2.0-green.svg)](https://opensource.org/licenses/Apache-2.0)'
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+    case ' GNU GPL v3.0':
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-black.svg)](https://www.gnu.org/licenses/gpl-3.0)'
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+    case 'BSD 2-Clause License':
+      return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-tan.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+  
+    default: return ' '
+      
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
